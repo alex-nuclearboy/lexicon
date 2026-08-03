86743eab70b0a9571fcfe3a2d93f0b4ddfd638b8
Aleksander <alex.nuclearboy@gmail.com>
2026-08-03 15:51:54 +0200

    feat(accounts): add application access permission
    Define a proxy user model with a dedicated, translatable permission for controlling access to the application without creating a new database table.

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,21 @@
-from django.db import models
+"""Models and model-level permissions for the accounts application."""
 
-# Create your models here.
+from django.contrib.auth.models import User
+from django.utils.translation import gettext_lazy as _
+
+
+class ApplicationAccess(User):
+    """Define permission for access to the application."""
+
+    class Meta:  # pylint: disable=too-few-public-methods
+        """Configure the application access proxy model."""
+        proxy = True
+        default_permissions = ()
+        permissions = [
+            (
+                "access_application",
+                _("Can access the application"),
+            ),
+        ]
+        verbose_name = _("application access")
+        verbose_name_plural = _("application access")
